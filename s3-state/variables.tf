1a735c470f7094df303dac5f77fef2abe13b50fb
Haf <haf@protonmail.ch>
2025-11-17 20:24:09 +0100

    feat: s3-state module

---
+++ s3-state/variables.tf
@@ -0,0 +1,8 @@
+variable "prefix" {
+    description = <<-EOT
+      A prefix that will be prepended to the names of all created resources.
+
+      This will create a s3 bucket called $prefix-terraform-state and a dynamodb table called $prefix-terraform-state-locks
+    EOT
+    type = string
+}
